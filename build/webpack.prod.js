81753c0d066f2f38e678c16e8937ff122097842c
Senli <senli@lagou.com>
2019-09-26 17:28:39 +0800

    init

---
+++ build/webpack.prod.js
@@ -0,0 +1,9 @@
+const config = {
+    entry: {
+        pageOne: './src/pageOne/index.js',
+        pageTwo: './src/pageTwo/index.js',
+        pageThree: './src/pageThree/index.js'
+    }
+};
+
+module.exports = config;
